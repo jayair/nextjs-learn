43c62ec5d60188aa1e515af2f72e66b7632a14ac
Jay V <air@live.ca>
2023-11-20 19:52:06 -0500

    Adding login

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,10 +2,15 @@ import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { Metadata } from "next";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 
 export default async function Page({
   searchParams,
